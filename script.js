a1101f4a8c09fcc94dd8805e6677ba8a105cc7cb
Chris Williamson <cjwilliamson109@gmail.com>
2026-02-15 08:50:14 +0000

    added movile menu

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ document.querySelectorAll('nav a').forEach(anchor => {
         });
     });
 });
+
+const mobileNav = document.getElementById("mobile-nav");
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const mobileLinks = document.querySelectorAll("#mobile-menu a");
+
+hamburgerBtn.addEventListener("click", () => {
+    mobileNav.classList.toggle("open");
+
+    const expanded = hamburgerBtn.getAttribute("aria-expanded") === "true";
+    hamburgerBtn.setAttribute("aria-expanded", !expanded);
+});
+
+// Close menu when a link is clicked
+mobileLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        mobileNav.classList.remove("open");
+        hamburgerBtn.setAttribute("aria-expanded", "false");
+    });
+});
