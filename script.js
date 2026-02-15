b43af839a265e55414af465d28e4ede2df356bb3
Chris Williamson <cjwilliamson109@gmail.com>
2026-02-15 11:35:02 +0000

    feat: made a lot of changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('nav a').forEach(anchor => {
+document.querySelectorAll('nav.home-nav a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
